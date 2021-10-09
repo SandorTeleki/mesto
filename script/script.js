677cc89eb2f25f98262661cedfe726696b05bda6
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2021-10-09 17:41:39 +0200

    feature: delete icon

--- script/script.js
+++ script/script.js
@@ -10,6 +10,40 @@ const jobInput = popupEdit.querySelector("#description");
 const fullname = profileInfo.querySelector(".profile__title");
 const description = profileInfo.querySelector(".profile__description");
 
+
+const initialCards = [
+    {
+        name: 'Будапешт',
+        link: 'https://images.unsplash.com/photo-1511902467434-4677a533a674?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+    {
+        name: 'Мальдивы',
+        link: 'https://images.unsplash.com/photo-1547528114-f4daa226e256?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+    {
+        name: 'Бангкок',
+        link: 'https://images.unsplash.com/photo-1598970605070-a38a6ccd3a2d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+    {
+        name: 'Рио-де-Жанейро',
+        link: 'https://images.unsplash.com/photo-1591468069148-5cc79ed1caaa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+    {
+        name: 'Братислава',
+        link: 'https://images.unsplash.com/photo-1562779346-e43d30d2e647?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+    {
+        name: 'Дубай',
+        link: 'https://images.unsplash.com/photo-1518684079-3c830dcef090?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1'
+    },
+];
+
+
+document.querySelector('.card__reaction').addEventListener('click', function (evt) {
+    evt.target.classList.toggle('card__reaction_active');
+  }); //only working for first card ATM
+
+
 // function togglePopupEdit (event) {
 //     nameInput.value = fullname.textContent;
 //     jobInput.value = description.textContent;
@@ -43,4 +77,6 @@ profileEditButton.addEventListener('click', openPopupEdit);
 profileCloseButton.addEventListener('click', closePopupEdit);
 // popupInputs.addEventListener('submit', submitFormHandler); -- Doesn't work for some reason...
 formEdit.addEventListener('submit', submitFormHandler);
+
+
 
