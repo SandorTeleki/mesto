3a3821fe16b4d5feac4ee0ad861612d1f04e04cb
Sandro Teleki <88478941+SandorTeleki@users.noreply.github.com>
2021-10-15 10:10:54 +0200

    hotfix: refactored JS code

--- script/script.js
+++ script/script.js
@@ -20,29 +20,6 @@ const popupAddLink = document.querySelector("#add-link");
 const popupCaption = document.querySelector(".popup__caption");
 const popupImage = document.querySelector(".popup__image");
 
-
-// const profileInfo = document.querySelector(".profile__info");
-// const nameInput = popupEdit.querySelector("#fullname")
-// const jobInput = popupEdit.querySelector("#description");
-// const fullname = profileInfo.querySelector(".profile__title");
-// const description = profileInfo.querySelector(".profile__description");
-// const formEdit = popupEdit.querySelector(".edit-form");
-// const cardsList = document.querySelector(".cards__list");
-
-// const popupInputs = document.querySelector(".popup__inputs");
-// const card = document.querySelector(".card");
-// const cardDeleteButton = document.querySelector(".card__delete");
-
-// const fullPicture = document.querySelector(".popup__full-picture");
-// const popupFrame = fullPicture.querySelector(".popup__frame");
-// const popupImage = fullPicture.querySelector(".popup__image");
-// const popupCaption = fullPicture.querySelector(".popup__caption");
-// const popupCloseButton = fullPicture.querySelector(".popup__close-button");
-
-// const popupAddCaption = document.querySelector("#add-title");
-// const popupAddLink = document.querySelector("#add-link");
-
-
 const initialCards = [
     {
         name: 'Будапешт',
@@ -76,20 +53,11 @@ const openPopup = function (popup) {
     popup.classList.add('popup_opened');
 };
 
-// const openPopup = (popup) => {
-//     popup.clasList.add('.popup_opened')
-// };
-
-
 
 //Close Popup
 const closePopup = function (popup) {
     popup.classList.remove('popup_opened');
 };
-
-// const closePopup = (popup) => {
-//     popup.classList.remove('popup_opened');
-// };
 
 
 //Open the Edit Popup
@@ -108,11 +76,6 @@ function submitFormHandler(event) {
     closePopup(popupEdit);
 }
 
-// function togglePopupEdit (event) {
-//     nameInput.value = fullname.textContent;
-//     jobInput.value = description.textContent;
-//     popupEdit.classList.toggle('popup_opened');
-// } 
 
 
 //Remove card (trash icon)
@@ -123,10 +86,6 @@ function cardDeleteButton(chosenCard) {
     });
 };
 
-// cardDeleteButton.addEventListener('click', (evt) => {
-//     card.remove();
-// }); 
-
 
 //Card reactions (heart icon)
 function cardReactionButton(chosenCard) {
@@ -135,17 +94,13 @@ function cardReactionButton(chosenCard) {
     });
 };
 
-// document.querySelector('.card__reaction').addEventListener('click', function (evt) {
-//     evt.target.classList.toggle('card__reaction_active');
-//   }); 
-
 
 //Open full size picture (popup image)
 function popupFullSize(chosenCard) {
     chosenCard.querySelector('.card__picture').addEventListener('click', function (evt) {
-        popupCaption.innerText = evt.target.parentElement.closest('.card').querySelector('.card__title').textContent;
+        popupCaption.innerText =  evt.target.alt;
         popupImage.src = evt.target.src;
-        popupImage.alt = evt.target.parentElement.closest('.card').querySelector('.card__title').textContent;
+        popupImage.alt = evt.target.alt;
         openPopup(fullPicture);
     });
 };
@@ -154,7 +109,7 @@ function popupFullSize(chosenCard) {
 //Close Full size image (popup image) 
 popupImageCloseButton.addEventListener('click', function () {
     const closeEditCard = popupImageCloseButton.closest('.popup_full-picture');
-    closeEditCard.classList.remove('popup_opened');
+    closePopup(fullPicture);
 });
 
 
@@ -171,41 +126,6 @@ function createCard(name, link) {
     return chosenCard;
 };
 
-// Create Card function
-
-// function createCard(cardInfo) {
-//     const cardTemplate = document.querySelector(".card-template").content;
-//     const cardUnit = cardTemplate.querySelector(".card").cloneNode('true');
-//     const cardPicture = cardUnit.querySelector(".card__picture");
-//     const cardTitle = cardUnit.querySelector(".card__title");
-//     const cardReaction = cardUnit.querySelector(".card__reaction");
-//     const cardDelete = cardUnit.querySelector(".card__delete");
-
-    // Card Reaction 
-    // cardReaction.addEventListener('click', function(evt) {
-    //     evt.currentTarget.classList.toggle('card__reaction_active');
-    // });
-
-    // Card Delete
-    // cardDelete.addEventListener('click', function(evt) {
-    //     evt.cardUnit.remove();
-    // });
-
-    // Card Image
-//     cardPicture.addEventListener('click', () => {
-//         popupImage.setAttribute('src', cardInfo.link);
-//         popupImage.setAttribute('alt', cardInfo.name);
-//         popupCaption.textContent = cardInfo.name;
-//         openPopup(fullPicture);
-//     });
-
-//     popupImage.setAttribute('src', cardInfo.link);
-//     popupImage.setAttribute('alt', cardInfo.name);
-//     popupCaption.textContent = cardInfo.name;
-
-
-//     return cardUnit;
-// }
 
 
 //Adding cards
@@ -236,75 +156,3 @@ formEdit.addEventListener('submit', submitFormHandler);
 closeButton.addEventListener('click', () => closePopup(popupEdit));
 profileAddButton.addEventListener('click', () => openPopup(popupAdd));
 popupAddCloseButton.addEventListener('click', () => closePopup(popupAdd));
-
-//Works alone, but not working in conjunction with submitFormHandler...
-// profileEditButton.addEventListener('click', togglePopupEdit);
-// profileCloseButton.addEventListener('click', togglePopupEdit);
-
-
-
-//Old code that didn't work:
-
-
-//Add card
-
-// function addCard(cardInfo, cardHolder) {
-//     const card = createCard(cardInfo);
-//     cardHolder.prepend(card);
-// };
-
-
-
-
-// function openPopupEdit() {
-//     nameInput.value = fullname.textContent;
-//     jobInput.value = description.textContent;
-//     popupEdit.classList.add("popup_opened");
-// }
-
-// function closePopup() {
-//     popup.classList.remove("popup_opened");
-// }
-
-// function closePopupEdit() {
-//     popupEdit.classList.remove("popup_opened");
-// }
-
-
-// function openPopupAdd() {
-//     popupAdd.classList.add("popup_opened");
-// }
-
-// function closePopupAdd() {
-//     popupAdd.classList.remove("popup_opened");
-// }
-
-
-
-// function submitFormHandler(event) {
-//     event.preventDefault();
-//     fullname.textContent = nameInput.value;
-//     description.textContent = jobInput.value;
-//     popupEdit.classList.remove("popup_opened");
-// }
-
-// profileEditButton.addEventListener('click', openPopupEdit);
-// // closeButton.addEventListener('click', closePopupEdit); ~~ Old version from sprint 4
-
-// editCloseButton.addEventListener('click', closePopupEdit);
-
-// profileAddButton.addEventListener('click', openPopupAdd);
-// addCloseButton.addEventListener('click', closePopupAdd);
-
-
-// closeButton.addEventListener('click', closePopupAdd); //need to make it for "popup close button, not profile close button"
-// popupInputs.addEventListener('submit', submitFormHandler); -- Doesn't work for some reason...
-// formEdit.addEventListener('submit', submitFormHandler);
-
-// initialCards.forEach(card => {
-//     addCard(card, cardsList);
-// });
-
-
-
-
